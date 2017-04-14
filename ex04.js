c8eb3bcb86dc4477cb24c3c5e4fedf324f2eca65
Massimiliano <mozz76@gmail.com>
2017-04-14 17:19:57 +0200

    from ex01 to ex04 done

--- ex04.js
+++ ex04.js
@@ -13,3 +13,68 @@
         editStringAt("toto", 3, "a"); // "tota";
 
 */
+
+var strA = "";
+var strB = "";
+
+function editStringAt(str, position, char) {
+
+	var i = 0;
+	
+	while (i < position) {
+		strA = strA + str[i];
+		i++;
+	}
+
+	i++;
+
+	while (i < str.length) {
+
+		strB = strB + str[i];
+		i++;
+	}
+
+	var outString = strA + char + strB;
+	
+	return outString;
+
+}
+
+/*
+	The following one will not work because of the 63rd
+	line.
+	Surpri
+*/
+
+/*
+var str = "vaffanculo";
+var position = 3;
+var chr = "X";
+
+function editStringAt(str, position, chr) {
+
+	var outStr = "";
+
+	var len = str.length;
+
+	var i = 0;
+	
+	while (i < len) {
+	
+		if (i === position) {
+			str[i] = chr;
+			console.log(str[i], len, chr);
+		}
+	
+		console.log(outStr, outStr.length);
+
+		outStr = outStr + str[i];
+
+		i++;
+	
+	}
+
+	return outStr;
+
+}
+*/
