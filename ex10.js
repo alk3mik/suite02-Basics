be08de37703f6c69ad6f92ebed7524c3be35b1bf
Massimiliano <mozz76@gmail.com>
2017-04-15 18:57:20 +0200

    The prototype of ex10.js was corrected from 'void' to 'arr'

--- ex10.js
+++ ex10.js
@@ -7,7 +7,7 @@
     Nous ne passerons que des nombres entiers positifs.
 
     Prototype:
-        void doubleSortNumbers(arr1, arr2);
+        arr doubleSortNumbers(arr1, arr2);
 
     Exemples:
         doubleSortNumbers([4, 3, 5, 2], [6, 3, 2]); // [2, 2, 3, 3, 4, 5, 6]
