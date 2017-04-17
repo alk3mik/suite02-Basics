4a91d00f1f763ae007add3da61faa14adbaecc10
Massimiliano <mozz76@gmail.com>
2017-04-17 03:52:18 +0200

    ex11.js solved

--- ex11.js
+++ ex11.js
@@ -11,3 +11,34 @@
         insertAt(["Bonjour", "va?"], "ça", 1); // ["Bonjour", "ça", "va?"]
 
 */
+
+function insertAt(arr, occurence, position) {
+
+	var bkpArr = [];
+	var len = arr.length;
+	var i = 0;
+
+	i = len;
+	while (i > position) {
+		bkpArr.push(arr.pop());
+		i--;
+	}
+
+	arr.push(occurence);
+
+	i = len - 1 - position;
+	while (i >= 0) {
+		arr.push(bkpArr[i]);
+		i--;
+	}
+
+	return arr;
+
+}
+/*
+var myArr = [4, "pippo", 8, 666];
+console.log(myArr);
+var res = insertAt(myArr, "pizza", 1);
+console.log(myArr);
+console.log(res);
+*/
